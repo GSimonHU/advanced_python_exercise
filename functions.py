fb8b71f17e794d3191446719ed596d39c43ab771
Gabor Simon <simon.gabor87hu@gmail.com>
2020-09-07 08:54:37 +0200

    Complete tasks

--- functions.py
+++ functions.py
@@ -1,18 +1,12 @@
+import collections
+
+
 def parse_user_data(line):
     """
     >>> parse_user_data('John Doe john.doe@example.com')
     ('John', 'Doe', 'john.doe', 'example.com')
     """
-
-    parts = line.split(' ')
-    first_name = parts[0]
-    last_name = parts[1]
-    email = parts[2]
-
-    email_parts = email.split('@')
-    user = email_parts[0]
-    host = email_parts[1]
-
+    first_name, last_name, user, host = line.replace('@', ' ').split(' ')
     response = (first_name, last_name, user, host)
     return response
 
@@ -25,16 +19,8 @@ def compare_lists(dir_a, dir_b):
     {'removed': ['hello.py'], 'added': ['hello2.py', 'install.txt']}
     """
 
-    removed = []
-    for filename in dir_a:
-        if filename not in dir_b:
-            removed.append(filename)
-
-    added = []
-    for filename in dir_b:
-        if filename not in dir_a:
-            added.append(filename)
-
+    removed = [filename for filename in dir_a if filename not in dir_b]
+    added = [filename for filename in dir_b if filename not in dir_a]
     return {'removed': sorted(removed), 'added': sorted(added)}
 
 
@@ -44,24 +30,8 @@ def print_log(message, process_id, timestamp, level=2):
     >>> print_log('System started!', 1532, datetime(2019, 1, 2, 10, 30, 55).isoformat(' '))
     2019-01-02 10:30:55 [1532] [INFO] System started!
     """
-
-    line = timestamp
-    line += ' [' + str(process_id) + ']'
-    if level == 0:
-        loglevel = 'TRACE'
-    elif level == 1:
-        loglevel = 'DEBUG'
-    elif level == 2:
-        loglevel = 'INFO'
-    elif level == 3:
-        loglevel = 'WARN'
-    elif level == 4:
-        loglevel = 'ERROR'
-    else:
-        loglevel = 'None'
-    line += ' [' + loglevel + ']'
-    line += ' ' + message
-    print(line)
+    loglevel = {0: 'TRACE', 1: 'DEBUG', 2: 'INFO', 3: 'WARN', 4: 'ERROR'}.get(level)
+    print(f'{timestamp} [{str(process_id)}] [{loglevel}] {message}')
 
 
 def biggest_rectangle(rectangles):
@@ -72,12 +42,10 @@ def biggest_rectangle(rectangles):
     >>> biggest_rectangle([(2, 4), (3, 3), (4, 2)])
     (3, 3)
     """
+    return [side for side in rectangles if side[0]*side[1] == max([side[0]*side[1] for side in rectangles])][0]
 
-    best = rectangles[0]
-    for r in rectangles:
-        if r[0] * r[1] > best[0] * best[1]:
-            best = r
-    return best
+    # rectangles.sort(key=max)
+    # return rectangles[0]
 
 
 def find_in_file(pattern, filename):
@@ -98,25 +66,10 @@ def find_in_file(pattern, filename):
     132 Quoth the Raven, "Nevermore."
     140 Shall be lifted- nevermore!
     """
-
-    f = open(filename)
-    try:
-        line_num = 0
-        for line in f:
-            char_num = 0
-            while line[char_num] == ' ':
-                char_num = char_num + 1
-            line = line[char_num: -1]
-            if pattern.lower() in line.lower():
-                spaces = ''
-                if line_num < 10:
-                    spaces = '  '
-                elif line_num < 100:
-                    spaces = ' '
-                print(spaces + str(line_num) + ' ' + line)
-            line_num += 1
-    finally:
-        f.close()
+    with open(filename) as file:
+        lines = file.readlines()
+        solution = list(enumerate([line.strip() for line in lines]))
+        print('\n'.join([' '.join((str(line[0]).rjust(3), line[1])) for line in solution if pattern.lower() in line[1].lower()]))
 
 
 def read_long_words(filename, min_length=0):
@@ -125,24 +78,9 @@ def read_long_words(filename, min_length=0):
     >>> words[:6]
     ['midnight', 'dreary', 'pondered', 'quaint', 'curious', 'volume']
     """
-
-    f = open(filename)
-    try:
-        content = f.read()
-    finally:
-        f.close()
-
-    # Remove punctuation
-    no_punct = []
-    for ch in content:
-        if ch not in r'[.,"!-]':
-            no_punct.append(ch)
-    content = ''.join(no_punct)
-    result = []
-    for word in content.split():
-        if len(word) > min_length:
-            result.append(word.lower())
-    return result
+    with open(filename) as file:
+        text = file.read()
+        return [word.strip('\n.,"!-').lower() for word in text.split(' ') if len(word.strip('\n.,"!-')) > min_length]
 
 
 def top_words(words, n=10):
@@ -154,18 +92,5 @@ def top_words(words, n=10):
     [('chamber', 11), ('nevermore', 10), ('lenore', 8), ('nothing', 7), ('tapping', 5)]
     """
 
-    word_counts = {}
-    for word in words:
-        if word in word_counts:
-            word_counts[word] += 1
-        else:
-            word_counts[word] = 1
-
-    result = []
-    for word, count in word_counts.items():
-        # Append (count, word) so that we sort by count.
-        result.append((count, word))
-
-    result.sort(reverse=True)
-    result = result[:n]
-    return [(count, word) for (word, count) in result]
+    words = sorted([(word, collections.Counter(words)[word]) for word in set(words)], key=lambda x: x[1], reverse=True)
+    return words[:n]
